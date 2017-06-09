0b03f999deab2290b759f2e22365644b97dda3ea
kainzigartig <kainz356@gmail.com>
2017-06-09 09:35:48 +0200

    Initial commit

---
+++ src/main/java/at/htld/module/bsp17/Bsp17FahrkameradApplication.java
@@ -0,0 +1,12 @@
+package at.htld.module.bsp17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bsp17FahrkameradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bsp17FahrkameradApplication.class, args);
+	}
+}
---
+++ src/test/java/at/htld/module/bsp17/Bsp17FahrkameradApplicationTests.java
@@ -0,0 +1,16 @@
+package at.htld.module.bsp17;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bsp17FahrkameradApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
